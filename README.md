833fbf12af84df40a1f9f6d707efd31e5d842bbe
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2019-01-21 09:06:51 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Flowable unit test example
+
+This project shows how to unit test [Flowable](https://www.flowable.org/) processes.
---
+++ src/main/kotlin/com/lifeinide/flowable/FlowableTestApplication.kt
@@ -0,0 +1,12 @@
+package com.lifeinide.flowable
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FlowableTestApplication
+
+fun main(args: Array<String>) {
+    runApplication<FlowableTestApplication>(*args)
+}
+
