12c0bdd9c0d7895c1a2fc58823d791e7c4779c25
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2019-01-25 07:30:40 +0100

    deploy processes

--- src/main/kotlin/com/lifeinide/flowable/FlowableTestApplication.kt
+++ src/main/kotlin/com/lifeinide/flowable/FlowableTestApplication.kt
@@ -3,6 +3,8 @@ package com.lifeinide.flowable
 import org.flowable.engine.RepositoryService
 import org.flowable.engine.RuntimeService
 import org.flowable.engine.TaskService
+import org.flowable.spring.SpringProcessEngineConfiguration
+import org.flowable.spring.boot.EngineConfigurationConfigurer
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.boot.CommandLineRunner
@@ -17,6 +19,12 @@ import org.springframework.context.annotation.Bean
 @SpringBootApplication
 class FlowableTestApplication {
 
+    /** Flowable configuration bean */
+    @Bean
+    fun flowableSpringProcessEngineConfig() = EngineConfigurationConfigurer<SpringProcessEngineConfiguration> {
+    }
+
+    /** Bean to only log how many processes we've succesfully deployed */
     @Bean
     fun init(repositoryService: RepositoryService,
              runtimeService: RuntimeService,
