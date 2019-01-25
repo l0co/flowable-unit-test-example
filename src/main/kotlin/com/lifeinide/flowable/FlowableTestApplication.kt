5d8ae623554d3de10b9a9905e15c2d7170057fbf
Łukasz Frankowski <lukasz.frankowski@gmail.com>
2019-01-25 07:18:05 +0100

    log processes found during deployment

--- src/main/kotlin/com/lifeinide/flowable/FlowableTestApplication.kt
+++ src/main/kotlin/com/lifeinide/flowable/FlowableTestApplication.kt
@@ -1,13 +1,38 @@
 package com.lifeinide.flowable
 
+import org.flowable.engine.RepositoryService
+import org.flowable.engine.RuntimeService
+import org.flowable.engine.TaskService
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+
 
 /**
  * @author Lukasz Frankowski
  */
 @SpringBootApplication
-class FlowableTestApplication
+class FlowableTestApplication {
+
+    @Bean
+    fun init(repositoryService: RepositoryService,
+             runtimeService: RuntimeService,
+             taskService: TaskService): CommandLineRunner {
+
+        return CommandLineRunner {
+            logger.debug("Found ${repositoryService.createProcessDefinitionQuery().list().size} processes")
+        }
+    }
+
+    companion object {
+        @JvmStatic val logger: Logger = LoggerFactory.getLogger(FlowableTestApplication::class.java)
+    }
+
+
+}
 
 fun main(args: Array<String>) {
     runApplication<FlowableTestApplication>(*args)
